ff7759376a13ca9d92fd7cae7da5278f0bfd563e
zeel-stack <zeel@clevertap.com>
2026-06-17 11:28:04 +0530

    Fix folder casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import CreateUser from "./components/CreateUser";
 import Notification from "./components/Notification";
-import NativeDisplay from "./Components/NativeDisplay";
-import Navbar from "./Components/Navbar";
+import NativeDisplay from "./components/NativeDisplay";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [activeTab, setActiveTab] = useState("create-user");
